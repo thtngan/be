efae624b2ee20d60e3f7a3323384666d0cb8e3a9
ThQuang21 <thienquang213@gmail.com>
2023-11-10 21:43:08 +0700

    Add verify JWT on user APIs

--- app/controllers/authControllers.js
+++ app/controllers/authControllers.js
@@ -85,4 +85,15 @@ async function signIn(req, res) {
     }
 }
 
-module.exports = { signUp, signIn };
+function checkAuth(req, res, next) {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        req.userData = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+}
+
+module.exports = { signUp, signIn, checkAuth };
--- app/routes/userEndpoint.js
+++ app/routes/userEndpoint.js
@@ -2,9 +2,10 @@ const express = require("express");
 const bcrypt = require('bcrypt');
 const User = require('../models/user')
 const router = express.Router();
+const auth = require('../controllers/authControllers')
 
 
-router.get('/:email', async (req, res) => {
+router.get('/:email', auth.checkAuth, async (req, res) => {
     try {
         const email = req.params.email;
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -23,28 +24,22 @@ router.get('/:email', async (req, res) => {
     }
 });
 
-//Update user
-router.put('/:email', async (req, res) => {
+router.put('/:email', auth.checkAuth, async (req, res) => {
     try{
        
         const email = req.params.email;
-
-        // Validate email format
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailRegex.test(email)) {
             return res.status(400).json({ error: 'Invalid email format' });
         }
 
-        // Find the user by email in the database
         const userToUpdate = await User.findOne({ email });
 
         if (!userToUpdate) {
             return res.status(404).json({ error: 'User not found' });
         }
 
-        // Update user fields if provided in the request body
         if (req.body.name) {
-            // Validate name length
             const maxLength = 25;
             if (req.body.name.length > maxLength) {
                 return res.status(400).json({ error: `Name should not be longer than ${maxLength} characters` });
@@ -52,7 +47,6 @@ router.put('/:email', async (req, res) => {
             userToUpdate.name = req.body.name;
         }
         if (req.body.age) {
-            // Validate age
             const age = parseInt(req.body.age);
             if (isNaN(age) || age <= 0 || age >= 150) {
                 return res.status(400).json({ error: 'Invalid age value' });
@@ -60,7 +54,6 @@ router.put('/:email', async (req, res) => {
             userToUpdate.age = age;
         }
         if (req.body.status) {
-            // Validate status value
             const validStatusValues = ['Married', 'Single', 'Divorced'];
             if (!validStatusValues.includes(req.body.status)) {
                 return res.status(400).json({ error: 'Invalid status value' });
@@ -68,7 +61,6 @@ router.put('/:email', async (req, res) => {
             userToUpdate.status = req.body.status;
         }
         if (req.body.job) {
-            // Validate job length
             const maxLength = 25;
             if (req.body.job.length > maxLength) {
                 return res.status(400).json({ error: `Job should not be longer than ${maxLength} characters` });
@@ -76,7 +68,6 @@ router.put('/:email', async (req, res) => {
             userToUpdate.job = req.body.job;
         }
         if (req.body.phone) {
-            // Validate phone length
             const maxLength = 25;
             if (req.body.phone.length > maxLength) {
                 return res.status(400).json({ error: `Phone should not be longer than ${maxLength} characters` });
@@ -84,7 +75,6 @@ router.put('/:email', async (req, res) => {
             userToUpdate.phone = req.body.phone;
         }
         if (req.body.city) {
-            // Validate city length
             const maxLength = 25;
             if (req.body.city.length > maxLength) {
                 return res.status(400).json({ error: `City should not be longer than ${maxLength} characters` });
@@ -92,16 +82,12 @@ router.put('/:email', async (req, res) => {
             userToUpdate.city = req.body.city;
         }
 
-        // Update the password if provided in the request body
         if (req.body.password) {
-            // Hash the new password before updating it
             const hashedPassword = await bcrypt.hash(req.body.password, 10);
             userToUpdate.password = hashedPassword;
         }
 
-        // Save the updated user data
         const updatedUser = await userToUpdate.save();
-
         res.status(200).json(updatedUser);
     }
     catch (error) {
